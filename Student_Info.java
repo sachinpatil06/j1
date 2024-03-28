cd17083a261ae537d02a2ab4fa13a9ca5eba87d9
sachin <01fe22bca023@kletech.ac.in>
2024-03-28 19:37:54 +0530

    student information

---
+++ Student_Info.java
@@ -0,0 +1,40 @@
+public class Student_Info{
+
+String name="Sachin patil";
+
+String department="BCA";
+
+String program="BCA";
+
+public String RegistertedCourse[]={" ", "C#", "Applied statistics", "Java"};
+
+public void display(){
+
+System.out.println("--Student Details----");
+
+System.out.println("Name:"+name);
+
+System.out.println("Department: "+department);
+
+System.out.println("Program: "+program);
+
+System.out.print( "Registered Courses:\n");
+
+for(int i=1;i<=3;i++){
+
+System.out.println(i+":"+RegistertedCourse[i]);
+
+}
+
+}
+
+
+public static void main(String[] args){
+
+Student_Info s = new Student_Info();
+
+s.display();
+}
+}
+
+
